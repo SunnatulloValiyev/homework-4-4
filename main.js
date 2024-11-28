2dfe80c88487ed991ffd3c7023b8d73ea434d1dc
SunnatulloValiyev <sunnatillovaliyev769@gmail.com>
2024-11-28 22:02:29 +0500

    commit

---
+++ main.js
@@ -0,0 +1,475 @@
+// 1-misol
+
+// for (let i = 1; i <=100; i++){
+//     console.log(i);
+// }
+
+// 2-misol
+
+// for (let i = 1; i <= 50; i++){
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 3-misol
+
+// for (let i = 1; i <= 30; i++){
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+// }
+
+
+// 4-misol
+
+// for (let i = 1; i <= 20; i++){
+//     console.log(i ** 2);
+// }
+
+
+// 5-misol
+
+// for (let i = 10; i >= 1; i--){
+//     console.log(i)
+// }
+
+// 6-misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++){
+//     sum += i
+//     console.log(sum);
+// }
+
+
+// 7-dars
+
+// for (let i = 1; i <= 20; i++){
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 8-vazifa
+
+// let sum = 1;
+
+// for (let i = 1; i <= 15; i++){
+//     if (i % 2 == 1) {
+//         sum *= i;
+//         console.log(sum);
+//     }
+// }
+
+// 9-misol
+
+// for (let i = 5; i <= 25; i++){
+//     console.log(i ** 3);
+// }
+
+
+// 10-misol
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(Math.sqrt(i));
+// }
+
+
+// 11-misol
+
+// for (let i = 1; i <= 50; i++){
+//     if (i % 3 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// 13-misol
+
+// for (let i = 1; i <= 100; i++){
+//     if (i % 7 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 14-misol
+
+// let sum = 0;
+
+// let count = 100;
+
+// for (let i = 1; i <= 100; i++) {
+//     sum += i;
+// }
+
+// let num = sum / count; 
+
+// console.log("Yig'indi:", sum);     
+// console.log("O'rtacha qiymat:", num); 
+
+
+// 15-misol
+
+// let num = 0;
+
+// for (i = 1; i <= 20; i++){
+//     if (i % 2 == 1) {
+//         num += i
+//         console.log(num);
+//     }
+// }
+
+
+// 16-misol
+
+// let num = +prompt("Son kiriting:")
+
+// for (let i = 1; i < num; i++){
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 17-misol
+
+// for (let i = 1; i <= 50; i++){
+//     if (i % 5 == 0 && i % 3 == 0)
+//         console.log("5 ga va 3 ga bo'linadigan sonlar" ,i);
+// }
+
+// 18-misol
+
+// let num = 0;
+
+// for (let i = 1; i <= 30; i++) {
+//     console.log("1 dan 30 gacha sonlar kvadrati", i ** 2);
+//     num += i; 
+// }
+
+// console.log("Yig‘indi:", num); 
+
+
+// 19-misol
+ 
+// let count = 0;
+
+// for (let i = 1; i <= 100; i++){
+//     if (i % 2 == 1) {
+//         count++
+//         console.log("Toq sonlar", i);
+//     }
+// }
+// console.log("toq sonlar soni", count);
+
+
+// 20-misol
+
+// let a = +prompt("Son kiriting:")
+// let b = +prompt("Son kiriting")
+
+// let res1 = Math.min(a, b);
+// let res2 = Math.max(a, b);
+
+// let num = 0;
+
+// for (let i = res1; i <= res2; i++){
+//     num += i;
+// }
+// console.log("1 son va 2 son ortasidagi yigindilar:", num);
+
+
+// 21-misol
+
+// for (let i = 1; i <= 20; i++){
+//     if(i % 2 == 1){
+//         console.log(i);
+//     }
+//     else{
+//         console.log("juft son");
+//     }
+// }
+
+// 23-misol
+
+// let num = 0;
+
+// for (let i = 0; i <= 100; i++){
+//     if(i % 10 == 0){
+//         console.log(null)
+//     }
+//     else {
+//         num += i;
+//         console.log(num);
+//     }
+// }
+
+
+// 24-misol
+
+// for (let i = 1; i <= 30; i++){
+//     console.log(i * 2);
+// }
+
+
+// 25-misol
+
+// let num1 = +prompt("Boshlang'ich son kiriting")
+// let num2 = +prompt("TRugash son kiriting son kiriting")
+
+// for (let i = num1; i <= num2; i++){
+//     console.log(i);
+// }
+
+
+// 26-misol
+
+// for (let i = 1; i <= 100; i++){
+//     if (i % 3 == 0 && i % 5 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+// 27-misol
+
+// let num = +prompt("Son kiriting:")
+// let num2 = 1;
+
+// for (let i = 1; i <= num; i++){
+//     num2 *= i;
+// }
+// console.log(num2)
+
+
+// 28-misol
+
+// for (let i = 1; i <= 50; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+        
+//     }
+// }
+
+
+// 29-misol
+
+// let num = 0;
+
+// for (let i = 1; i <= 10; i++){
+//     console.log("1 dan 10 gacha sonlar kublari", i ** 3);
+//     num += i ** 3;
+//     console.log("yigindi", num)
+// }
+
+
+// 30-misol
+
+// for (let i = 1; i <= 20; i++){
+//     console.log(`bu sonning kvadrati ${i}`);
+// }
+
+// 31-misol
+
+// for (let i = 1; i <= 100; i++){
+//     if (i % 7 == 0){
+//         console.log(i * 2);         
+//     }
+// }
+
+
+// 32-misol
+// for (let i = 0; i <= 30; i+=10){
+//     console.log(i * 10);
+// }
+
+// 33 misol hato berilgan shekili domla ishlolmadim 22 misol ham shunday ekan
+
+// 34-misol
+
+// let num = +prompt("Son kiriting:")
+
+// for (let i = 1; i <= num; i++){
+//     if (i % 2 == 0) {
+//         console.log(i += i);
+//     }
+// }
+
+
+// 35-misol
+
+// let num1 = +prompt("Son kiriting:")
+// let num2 = +prompt("Son kiriting:")
+// let num3 = 1;
+
+// for(let i = num1; i <= num2; i++){
+//     num3 *= i;
+// }
+// console.log(num3);
+
+// 36-misol
+
+// let num1 = +prompt("Son kirting")
+
+
+// for (let i = 1; i <= num1; i++){
+//     if(i % 10 == 0){
+//         num1 + i;
+//         console.log(num1 + 10);
+//     }
+// }
+
+
+// 37-misol
+
+// let num = 0;
+
+// for (let i = 1; i <=15; i++){
+//     console.log(i ** 2);    
+//     num += i ** 2;
+// }
+// console.log(num);
+
+
+// 38-misol
+
+// let num = 1;
+
+// for (let i = 1; i <= 10; i++){
+//     num *= i;
+//     console.log(num);
+// }
+
+
+// 39-misol ni ishlolmadim domla 
+
+// 40-misol
+// let number = +prompt("musbat son kiriting:");
+// if (number > 0) {
+//   for (let i = number - 1; i > 0; i--) {
+//     console.log(i);
+//   }
+// } else {
+//   console.log("Iltimos, musbat son kiriting!");
+// }
+
+
+// 41-misol
+
+// for (let i = 1; i <= 50; i++) {
+//     let cube = i ** 3;
+//     if (cube % 2 === 0) {
+//       console.log(`${i} = ${cube}`);
+//     }
+//   }
+
+// 42-misol
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 5 !== 0) {
+//       console.log(i);
+//     }
+//   }
+  
+  
+// 43-misol
+
+// let number = +prompt("musbat son kiriting:");
+// let sum = 0;
+
+// if (number > 0) {
+//   for (let i = 1; i < number; i++) {
+//     console.log(i);
+//     if (i % 3 === 0) {
+//       sum += i;
+//     }
+//   }
+//   console.log(`3 ga karralilarning yig'indisi: ${sum}`);
+// }
+
+
+// 44-misol
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//       console.log(`${i} - Juft son`);
+//     } else {
+//       console.log(`${i} - Toq son`);
+//     }
+//   }
+  
+
+
+// 45-misol
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 !== 0) {
+//     let square = i ** 2;
+//     console.log(`${i}^2 = ${square}`);
+//     sum += square;
+//   }
+// }
+
+// console.log(`Toq sonlarning kvadratlari yig'indisi: ${sum}`);
+
+
+// 47-misol
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 3 === 0 && i % 4 === 0) {
+//     console.log(i);
+//     sum += i;
+//   }
+// }
+
+// console.log(`3 va 4 ga bo'linadigan sonlarning yig'indisi: ${sum}`);
+
+
+// 48-misol
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 !== 0) {
+//       console.log(i);
+//     }
+//   }
+  
+
+
+// 49-misol
+
+// let number = +prompt("on kiriting:");
+// let product = 1;
+
+// if (number > 0) {
+//   for (let i = 7; i < number; i += 7) {
+//     product *= i;
+//   }
+//   console.log(`7 ga karrali sonlarning ko'paytmasi: ${product}`);
+// } else {
+//   console.log("Iltimos, musbat son kiriting!");
+// }
+
+
+// 50-misol
+
+// let num1 = +prompt("Boshlang'ich sonni kiriting:");
+// let num2 = +prompt("Tugash sonini kiriting:");
+// let sum = 0;
+
+// if (num1 > 0 && num2 > 0 && num1 < num2) {
+//   for (let i = num1; i <= num2; i++) {
+//     let square = i ** 2;
+//     console.log(`${i}^2 = ${square}`);
+//     sum += square;
+//   }
+
+//   console.log(`Kvadratlarning yig'indisi: ${sum}`);
+
+// } 
